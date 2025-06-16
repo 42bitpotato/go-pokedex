d79aadf2104571f4bcfebbf347e307322c10bdff
128bitpotato <skickligt01.flagig@icloud.com>
2025-06-16 16:38:46 +0200

    moved cleanInput

--- main.go
+++ main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-	"strings"
-)
-
 func main() {
-	fmt.Println("Hello, World!")
-}
 
-func cleanInput(text string) ([]string, error) {
-	text = strings.TrimSpace(text)
-
-	// Check if only whitespaces
-	if text == "" {
-		return nil, fmt.Errorf("input is blank or only whitespace")
-	}
-	// Check if non-alphanumeric
-	reAlpha := regexp.MustCompile(`^[A-Za-z\s]+$`)
-	if !reAlpha.MatchString(text) {
-		return nil, fmt.Errorf("input containing non-alphanumeric characters: %s", text)
-	}
-
-	text = strings.ToLower(text)
-
-	re, err := regexp.Compile(`\s+`)
-	if err != nil {
-		return nil, fmt.Errorf("invalid regex: %w", err)
-	}
-	words := re.Split(text, -1)
-
-	return words, nil
 }

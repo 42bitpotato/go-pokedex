de1706a3719e3210fc8f25f68111d185fdfe989e
42bitpotato <skickligt01.flagig@icloud.com>
2025-06-29 10:17:03 +0200

    Added map command an module

--- repl.go
+++ repl.go
@@ -76,5 +76,10 @@ func getCommands() map[string]cliCommand {
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
+		"map": {
+			name:        "map",
+			description: "Display the names of 20 location areas in the Pokemon world",
+			callback:    commandMap,
+		},
 	}
 }

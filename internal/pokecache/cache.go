5a3d49ee4289c7a0ac40344cf29736dda0b0bf43
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-03 06:28:07 +0200

    setting up caching

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -1,6 +1,27 @@
 package pokecache
 
+import (
+	"sync"
+	"time"
+)
+
 type cacheEntry struct {
-	createdAt int
+	createdAt time.Time
 	val       []byte
+}
+
+type Cache struct {
+	entries map[string]cacheEntry
+	mu      sync.Mutex
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	entry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.entries[key] = entry
 }

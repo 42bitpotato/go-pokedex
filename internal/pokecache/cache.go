279797aef369e8de84fdfd216b627e33681a1634
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-05 08:37:32 +0200

    Restructure code, move cache methods to separate file

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -14,23 +14,3 @@ type Cache struct {
 	entries map[string]cacheEntry
 	mu      sync.Mutex
 }
-
-func (c *Cache) Add(key string, val []byte) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	entry := cacheEntry{
-		createdAt: time.Now(),
-		val:       val,
-	}
-	c.entries[key] = entry
-}
-
-func (c *Cache) Get(key string) (val []byte, ok bool) {
-	entry, ok := c.entries[key]
-	if !ok {
-		return val, ok
-	}
-	val = entry.val
-	return val, ok
-}
---
+++ internal/pokecache/cache_methods.go
@@ -0,0 +1,23 @@
+package pokecache
+
+import "time"
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	entry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.entries[key] = entry
+}
+
+func (c *Cache) Get(key string) (val []byte, ok bool) {
+	entry, ok := c.entries[key]
+	if !ok {
+		return val, ok
+	}
+	val = entry.val
+	return val, ok
+}

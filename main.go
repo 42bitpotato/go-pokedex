31346fa43dfea61a55c0c37397653c4b17315049
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-05 13:47:23 +0200

    added cache interval config

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 type config struct {
-	mapNext     string
-	mapPrevious string
+	mapNext       string
+	mapPrevious   string
+	cacheInterval int
 }
 
 func main() {
 	cfg := &config{
-		mapNext:     "https://pokeapi.co/api/v2/location-area/?offset=0&limit=20",
-		mapPrevious: "",
+		mapNext:       "https://pokeapi.co/api/v2/location-area/?offset=0&limit=20",
+		mapPrevious:   "",
+		cacheInterval: 5,
 	}
 	startRepl(cfg)
 }

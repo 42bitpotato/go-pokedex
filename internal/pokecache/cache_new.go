eb773b6728ae4d09d4bc91856593707e01ed50b6
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-05 14:28:34 +0200

    updated to return pointer for cfg

--- internal/pokecache/cache_new.go
+++ internal/pokecache/cache_new.go
@@ -2,8 +2,9 @@ package pokecache
 
 import "time"
 
-func NewCache(interval int) {
-	var cache Cache
+func NewCache(interval int) *Cache {
+	cache := &Cache{}
 	timeInterval := time.Duration(interval) * time.Second
 	cache.reapLoop(timeInterval)
+	return cache
 }

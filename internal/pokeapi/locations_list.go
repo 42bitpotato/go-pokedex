a8e8464c8628c70be85de2bf2dd6afefc24fb6e8
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-05 22:58:10 +0200

    fixed faulte value syntax

--- internal/pokeapi/locations_list.go
+++ internal/pokeapi/locations_list.go
@@ -5,20 +5,28 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/42bitpotato/go-pokedex/internal/pokecache"
 )
 
-func ListLocations(url string) (mapRespons, error) {
-	// Get url response
-	res, err := http.Get(url)
-	if err != nil {
-		return mapRespons{}, fmt.Errorf("failed to fetch location areas: %w", err)
-	}
+func ListLocations(url string, cache *pokecache.Cache) (mapRespons, error) {
 
-	//Save response body to variable
-	defer res.Body.Close()
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return mapRespons{}, fmt.Errorf("failed to read location response: %w", err)
+	// Check if url in cache
+	data, ok := cache.Get(url)
+	if !ok {
+		// Get url response
+		res, err := http.Get(url)
+		if err != nil {
+			return mapRespons{}, fmt.Errorf("failed to fetch location areas: %w", err)
+		}
+
+		//Save response body to variable
+		defer res.Body.Close()
+		data, err = io.ReadAll(res.Body)
+		if err != nil {
+			return mapRespons{}, fmt.Errorf("failed to read location response: %w", err)
+		}
+
 	}
 
 	// Unmarshall json to struct
@@ -26,6 +34,6 @@ func ListLocations(url string) (mapRespons, error) {
 	if err := json.Unmarshal(data, &responsJson); err != nil {
 		return mapRespons{}, fmt.Errorf("failed to unmarshal response: %w", err)
 	}
-
+	cache.Add(url, data)
 	return responsJson, nil
 }

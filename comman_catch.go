509e1bd057b2c3ef51886c6ee467d7dc47c7a4d3
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-08 06:48:04 +0200

    added newlines to print

--- comman_catch.go
+++ comman_catch.go
@@ -14,13 +14,13 @@ func commandCatch(cfg *config, arg string) error {
 		return err
 	}
 
-	fmt.Printf("Throwing a Pokeball at %s...", pokemon.Name)
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
 	successCatch := catchChance(pokemon.BaseExp)
 	if !successCatch {
-		fmt.Printf("%s escaped!", pokemon.Name)
+		fmt.Printf("%s escaped!\n", pokemon.Name)
 		return nil
 	}
-	fmt.Printf("%s was caught!", pokemon.Name)
+	fmt.Printf("%s was caught!\n", pokemon.Name)
 	cfg.caughtPokemon[pokemon.Name] = pokemon
 	return nil
 }

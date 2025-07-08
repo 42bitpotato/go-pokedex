2ff2076f7de3ba28f1313a11cc26cb3d47ba7e15
42bitpotato <skickligt01.flagig@icloud.com>
2025-07-08 05:41:10 +0200

    rename for clarity

---
+++ internal/pokeapi/encounters_list.go
@@ -0,0 +1,39 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/42bitpotato/go-pokedex/internal/pokecache"
+)
+
+func ListEncounters(url string, cache *pokecache.Cache) (LocationEncounters, error) {
+
+	// Check if url in cache
+	data, ok := cache.Get(url)
+	if !ok {
+		// Get url response
+		res, err := http.Get(url)
+		if err != nil {
+			return LocationEncounters{}, fmt.Errorf("failed to fetch location areas: %w", err)
+		}
+
+		//Save response body to variable
+		defer res.Body.Close()
+		data, err = io.ReadAll(res.Body)
+		if err != nil {
+			return LocationEncounters{}, fmt.Errorf("failed to read location response: %w", err)
+		}
+
+	}
+
+	// Unmarshall json to struct
+	var responsJson LocationEncounters
+	if err := json.Unmarshal(data, &responsJson); err != nil {
+		return LocationEncounters{}, fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+	cache.Add(url, data)
+	return responsJson, nil
+}
